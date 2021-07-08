e3d4074d497fe08b9ff474486f2d95a141158055
HajarePratik <pratikhajare09@gmail.com>
2021-07-08 22:27:52 +0530

    [Pratik]Add : Create a Greeting App with Templete Format

---
+++ src/main/java/com/bridgelabz/demo/SpringBootMessagingAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMessagingAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Greeting Messaging App");
+		SpringApplication.run(SpringBootMessagingAppApplication.class, args);
+	}
+
+}
